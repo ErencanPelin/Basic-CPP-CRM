704c40503bddb6607d6ef4ee180cc7b04cd5f4cd
Erencan Pelin <erencan.pelin@gmail.com>
2022-08-08 10:05:16 +1000

    add ability to add customers

--- BasicCRM.cpp
+++ BasicCRM.cpp
@@ -1,11 +1,41 @@
-
 #include <iostream>
+#include "Customer.h"
+using namespace std;
+
+Customer customers[50];
+
+void AddCustomer()
+{
+    Customer new_customer;
+    cout << "What is your first name? " << endl;
+    cin >> new_customer.f_name;
+    cout << "What is your last name? " << endl;
+    cin >> new_customer.l_name;
+    cout << "What is your age? " << endl;
+    cin >> new_customer.age;
+    
+    cout << "Hi " << new_customer.f_name << " " << new_customer.l_name << ", nice to meet you.";
+    customers[0] = new_customer;
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Give me a number\n";
-    int test;
-    std::cin >> test;
-    std::cout << test;
+    cout << "=== Basic CRM ===" << endl;
+    char in;
+    cin >> in;
+    in = toupper(in);
+    switch (in)
+    {
+        case 'A':
+            {
+                AddCustomer();
+                break;
+            }
+        case 'X':
+            {
+                break;
+            }
+        default: break;
+    }
     return 0;
 }
---
+++ Customer.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Customer
+{
+public:
+    string f_name;
+    string l_name;
+    int age;
+};
